cfa3fec60fbc0a54455ec6d6c36360797552f55a
rachel <rachelkemuma99@gmail.com>
2022-07-20 10:35:35 +0300

    make migrations for class room,topc and message

--- base/models.py
+++ base/models.py
@@ -8,8 +8,8 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 class Room(models.Model):
-    # host =
-    topic= models.ForeignKey(Topic,on_delete=models.CASCADE)
+    host= models.ForeignKey(User,on_delete=models.CASCADE,default='')
+    topic= models.ForeignKey(Topic,on_delete=models.CASCADE,default='')
     name = models.CharField(max_length=200)
     description = models.TextField(null=True,blank=True)
     # participants =
