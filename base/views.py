e00b413df89bfbaa82090002556b8074fb31c024
rachel <rachelkemuma99@gmail.com>
2022-09-06 16:05:30 +0300

    import a login decorator

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
