069ea0ef25541c2a3f3d041d975872a4a0d78e71
rachel <rachelkemuma99@gmail.com>
2022-09-27 12:53:29 +0300

    create an abstract user and clear the database

--- base/forms.py
+++ base/forms.py
@@ -1,7 +1,7 @@
 from dataclasses import field
 from django.forms import ModelForm
 from .models import *
-from django.contrib.auth.models import User
+
 
 class RoomForm(ModelForm):
     class Meta:
--- base/views.py
+++ base/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import redirect, render
 from django.db.models import Q
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.forms import UserCreationForm 
 from django.contrib import messages
