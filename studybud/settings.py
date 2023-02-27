c0bdead2cfd3396ce2f9910a93ac5bbb6d3d7b6a
kema-ray <rachelkemuma99@gmail.com>
2023-02-27 12:40:58 +0300

    make changes to the settings file

--- studybud/settings.py
+++ studybud/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-40o+#10%r(2$6b4!08*^o2r%uu5e5z1gte^b&#mtuxaxdv9yf5'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
